1f68b09d8282a88d6b5720ef524e79b18af3434e
EvgenPEO <zheka12125@gmail.com>
2023-05-02 15:08:11 +0300

    done1

--- include/fun.h
+++ include/fun.h
@@ -1,6 +1,9 @@
 // Copyright 2022 UNN-IASR
 #ifndef INCLUDE_FUN_H_
 #define INCLUDE_FUN_H_
+#include <iostream>
+#include <cctype>
+#include <string>
 
 unsigned int faStr1(const char *str);
 unsigned int faStr2(const char *str);
