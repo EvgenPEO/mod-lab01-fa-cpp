6276d988281b12c9443523ae9c38c1efcb09579f
EvgenPEO <zheka12125@gmail.com>
2023-05-02 14:40:36 +0300

    done

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,88 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool isWord = false;
+    bool containsDigit = false;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (!isspace(str[i])) {
+            isWord = true;
+            if (isdigit(str[i])) {
+                containsDigit = true;
+            }
+        }
+        else {
+            if (isWord && !containsDigit) {
+                count++;
+            }
+            isWord = false;
+            containsDigit = false;
+        }
+    }
+
+    if (isWord && !containsDigit) {
+        count++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool isWord = false;
+    bool isValidWord = false;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (!isspace(str[i])) {
+            if (!isWord && isupper(str[i])) {
+                isValidWord = true;
+            }
+            else if (!islower(str[i])) {
+                isValidWord = false;
+            }
+            isWord = true;
+        }
+        else {
+            if (isWord && isValidWord) {
+                count++;
+            }
+            isWord = false;
+            isValidWord = false;
+        }
+    }
+
+    if (isWord && isValidWord) {
+        count++;
+    }
+
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    unsigned int wordCount = 0;
+    unsigned int totalLength = 0;
+    unsigned int currentWordLength = 0;
+    bool isWord = false;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (!isspace(str[i])) {
+            isWord = true;
+            currentWordLength++;
+        }
+        else {
+            if (isWord) {
+                wordCount++;
+                totalLength += currentWordLength;
+            }
+            isWord = false;
+            currentWordLength = 0;
+        }
+    }
+
+    if (isWord) {
+        wordCount++;
+        totalLength += currentWordLength;
+    }
+
+    return wordCount == 0 ? 0 : (totalLength + wordCount / 2) / wordCount;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,13 @@
 #include "fun.h"
 
 int main() {
+    const char* str1 = "Hello world123 42 test";
+    const char* str2 = "Hello World anotherWord Test notValid";
+    const char* str3 = "Hello world test string";
+
+    std::cout << "faStr1: " << faStr1(str1) << std::endl;
+    std::cout << "faStr2: " << faStr2(str2) << std::endl;
+    std::cout << "faStr3: " << faStr3(str3) << std::endl;
+
     return 0;
 }
